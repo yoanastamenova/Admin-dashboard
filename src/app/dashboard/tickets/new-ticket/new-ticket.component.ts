ceea673e62ef7652472545164e5ef27d26e6c28c
yoanastamenova <yoana.stamenovaa@gmail.com>
2024-11-11 16:41:12 +0100

    feat: added new component for controling forms and imported it in other components

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ControlComponent } from '../../../shared/control/control.component';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [],
+  imports: [ControlComponent],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
