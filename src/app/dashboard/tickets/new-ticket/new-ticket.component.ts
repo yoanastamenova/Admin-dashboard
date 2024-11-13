63a56d6655f743169d3e418a3088905e1a89ea18
yoanastamenova <yoana.stamenovaa@gmail.com>
2024-11-13 11:59:13 +0100

    fix: chsnged ViewChild to function use instead of predefined

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,21 +1,23 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ButtonComponent } from './../../../shared/button/button.component';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
-import { ButtonComponent } from '../../../shared/button/button.component';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ControlComponent, FormsModule],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form')
 
   onSubmit(title: string, text: string) {
     console.log(title);
     console.log(text);
-    this.form?.nativeElement.reset();
+
+    this.form().nativeElement.reset();
   }
 }
