552861f13b13add4ba4e9b51a2ad951183fd8367
yoanastamenova <yoana.stamenovaa@gmail.com>
2024-11-13 11:48:28 +0100

    feat: added tempalte variables in the new-ticket template and used them with the ViewChild as a function and as default decorator to access template variables

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
+import { ButtonComponent } from '../../../shared/button/button.component';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ControlComponent],
+  imports: [ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
 
+  onSubmit(title: string, text: string) {
+    console.log(title);
+    console.log(text);
+    this.form?.nativeElement.reset();
+  }
 }
