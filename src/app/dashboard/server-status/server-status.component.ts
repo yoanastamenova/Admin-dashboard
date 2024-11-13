709bd10fc1ebbba82c0fc3fe14dfa10082aa41e3
yoanastamenova <yoana.stamenovaa@gmail.com>
2024-11-13 14:06:13 +0100

    feat: used signals as effect

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -8,9 +8,14 @@ import { Component, DestroyRef, inject, OnInit } from '@angular/core';
   styleUrl: './server-status.component.css'
 })
 export class ServerStatusComponent implements OnInit{
-  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  currentStatus = signal<'online' | 'offline' | 'unknown'>('offline');
   private destroyRef = inject(DestroyRef);
-  constructor() {}
+  
+  constructor() {
+    effect(() => {
+      console.log(this.currentStatus());
+    });
+  }
 
   ngOnInit() {
     console.log('On init');
@@ -18,11 +23,11 @@ export class ServerStatusComponent implements OnInit{
       const rnd = Math.random();
 
       if(rnd < 0.5){
-        this.currentStatus = 'online';
+        this.currentStatus.set('online');
       } else if(rnd < 0.9) {
-        this.currentStatus = 'offline'
+        this.currentStatus.set('offline');
       } else {
-        this.currentStatus = 'unknown'
+        this.currentStatus.set('unknown');
       }
     }, 5000)
 
