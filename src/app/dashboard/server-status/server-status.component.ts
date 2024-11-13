376eb3bbd6132ef17f3781560b4732b922369c6c
yoanastamenova <yoana.stamenovaa@gmail.com>
2024-11-13 10:44:36 +0100

    feat: added more hooks such as ngAfterViewInit and OnDestroy with method and with injection usage

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -9,11 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerStatusComponent implements OnInit{
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
-
+  private destroyRef = inject(DestroyRef);
   constructor() {}
 
   ngOnInit() {
-    setInterval( () => {
+    console.log('On init');
+    const interval = setInterval(() => {
       const rnd = Math.random();
 
       if(rnd < 0.5){
@@ -24,6 +25,18 @@ export class ServerStatusComponent implements OnInit{
         this.currentStatus = 'unknown'
       }
     }, 5000)
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    })
   }
+
+  ngAfterViewInit() {
+    console.log('After view init');
+  }
+
+  // ngOnDestroy() {
+  //   clearTimeout(this.interval)
+  // }
 
 }
